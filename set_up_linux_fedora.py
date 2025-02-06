d21b9c19d9f5fd0272cbad0de768a96dd5db001b
Lemetist <Lemetist@gmail.com>
2025-02-07 00:07:52 +0300

    systemctl

--- set_up_linux_fedora.py
+++ set_up_linux_fedora.py
@@ -4,25 +4,27 @@ from common import (personal_projects_path_str, projects_path_str,
                     work_projects_path_str)
 
 
-def setup_linux_using_dnf():
+def setup_linux_using_dnf() :
     """
     Install must-have packages I use almost every day.
     """
-
     dnf.packages(
         packages=[
-           "docker",
+            "docker",
             "go",
             "nmap",
-            "postgresql-server"
-
+            "postgresql-server",
         ],
         _sudo=True
     )
 
+    server.shell(commands=[
+        "sudo systemctl enable --now docker",
+        "sudo systemctl enable --now postgresql"
+    ])
 
 
-def setup_basic_directories():
+def setup_basic_directories() :
     """
     Here I create basic directories I use every day.
     """
@@ -30,23 +32,33 @@ def setup_basic_directories():
         projects_path_str,
         personal_projects_path_str,
         work_projects_path_str
-    ]:
+    ] :
         files.directory(
             path=path,
             present=True,
+            _sudo=True
         )
 
 
-
-
-def setup_ohmyzsh():
+def setup_snap_packages() :
     """
-    No comments here, everybody knows this.
+    Install must-have packages using Snap.
     """
     server.shell(commands=[
-                 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" | grep -e "already exists" -e "is now installed"'])
+        "sudo snap install --classic code",
+        "sudo snap install --classic telegram-desktop"
+        "flatpak install flathub dev.vencord.Vesktop"
+    ])
+
+def setup_ohmyzsh() :
+    """
+    Install Oh My Zsh non-interactively.
+    """
+    server.shell(commands=[
+        'ZSH=${HOME}/.oh-my-zsh sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended'
+    ])
+
 
 setup_linux_using_dnf()
 setup_basic_directories()
-
 setup_ohmyzsh()
